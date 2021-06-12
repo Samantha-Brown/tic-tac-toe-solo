0b35cb8f84925e4bff6c1d1ef632d10422b921d3
Samantha Brown <bro7445@gmail.com>
2021-06-12 16:23:46 -0400

    Create the game class

---
+++ game.js
@@ -0,0 +1,14 @@
+class Game {
+  constructor(player1, player2) {
+    this.player1 = new Player; // which one??
+    this.player2 = player2;
+    this.turn = // just turn; or a player/tokin?
+    this.winningBoards = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9],[3,5,7]];
+  }
+}
+// will need methods for:
+//1. changing players,
+//2. logging tokins on board,
+// 3. determine winner of game, or if draw
+//4. displaying each players wins,
+//5. reseting board after game,
