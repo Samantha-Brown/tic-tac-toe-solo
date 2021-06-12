9062ee77603fb2f864a051d085f492b534c69164
Samantha Brown <bro7445@gmail.com>
2021-06-12 16:24:49 -0400

    Label sections and add window event listener

---
+++ main.js
@@ -0,0 +1,18 @@
+
+// QUERY SELECTORS
+
+var ;
+
+// GLOBAL VARIABLE
+
+var anotherGame = new Game();
+
+// EVENT LISTENERS
+
+window.addEventListener('load', newGame);
+
+// FUNCTIONS
+function newGame() {
+  // needs to clear/reset game board
+  // needs to invoke add win to player scoreboard
+}
