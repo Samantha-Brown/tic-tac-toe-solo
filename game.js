7dec223b9298778abaaf29fb026ceccf6243e5eb
Samantha Brown <bro7445@gmail.com>
2021-06-15 12:45:17 -0400

    Add checkForWin function

--- game.js
+++ game.js
@@ -20,10 +20,15 @@ class Game {
 
   }
   checkForWin() {
-
+    var winningSetup = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9],[3,5,7]];
+    for (var i = 0; i < this.winningBoards.length; i++) {
+      if (this.winningBoards[winningSetup[i][0]] === this.whoseTurn && this.winningBoards[winningSetup[i][1]] === this.whoseTurn && this.winningBoards[winningSetup[i][2]] === this.whoseTurn) {
+        return true;
+      }
+   }
   }
   resetBoard() {
-    if 
+    if
   }
 }
 
