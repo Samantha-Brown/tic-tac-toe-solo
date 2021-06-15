e4065c6d3bb6ffcc1cdc38b0e83b0f21a0f40ee7
Samantha Brown <bro7445@gmail.com>
2021-06-15 11:51:00 -0400

    Add newGame function and query selectors

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 
 // QUERY SELECTORS
 var topOfPageText = document.querySelector(".display-turn");
-var one = document.querySelector("1");
-var two = document.querySelector("2");
-var three = document.querySelector("3");
-var four = document.querySelector("4");
-var five = document.querySelector("5");
-var six = document.querySelector("6");
-var seven = document.querySelector("7");
-var eight = document.querySelector("8");
-var nine = document.querySelector("9");
+var one = document.querySelector("#1");
+var two = document.querySelector("#2");
+var three = document.querySelector("#3");
+var four = document.querySelector("#4");
+var five = document.querySelector("#5");
+var six = document.querySelector("#6");
+var seven = document.querySelector("#7");
+var eight = document.querySelector("#8");
+var nine = document.querySelector("#9");
 var gameBoard = document.querySelector("#game-board");
+var player1WinsDisplay = document.querySelector("#display-1-wins");
+var player2WinsDisplay = document.querySelector("#display-2-wins");
 // GLOBAL VARIABLE
 
 var anotherGame = new Game();
@@ -21,6 +23,16 @@ window.addEventListener("load", newGame);
 gameBoard.addEventListener("click", placeToken);
 // FUNCTIONS
 function newGame() {
+  if (localStorage["display-1-wins"] > 0) {
+    game.player1.retrieveWinsFromStorage();
+  };
+  if (localStorage["display-2-wins"] > 0) {
+    game.player2.retrieveWinsFromStorage();
+  };
+
+player1WinsDisplay.innerText = game.player1.wins || 0;
+player2WinsDisplay.innerText = game.player2.wins || 0;
+}
   // needs to clear/reset game board
   // needs to retrieve local storage of wins
 }
