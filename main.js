4bb1d77c115b6b35842b21f3bfc8c30ddae5f5cb
Samantha Brown <bro7445@gmail.com>
2021-06-15 11:25:25 -0400

    Add displayWhoseTurn function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 // QUERY SELECTORS
-
+var topOfPageText = document.querySelector(".display-turn");
 var one = document.querySelector("1");
 var two = document.querySelector("2");
 var three = document.querySelector("3");
@@ -24,8 +24,17 @@ function newGame() {
   // needs to clear/reset game board
   // needs to retrieve local storage of wins
 }
+
 function placeToken() {
 
+}
+
+function displayWhoseTurn() {
+  if (anotherGame.player1.turn === true) {
+    topOfPageText.innerHTML = `It's ${anotherGame.player1.id}'s turn!`
+  } else {
+    topOfPageText.innerHTML = `It's ${anotherGame.player2.id}'s turn!`
+  }
 }
 
 // Need function to Display who's turn it is, & who won or if draw...
