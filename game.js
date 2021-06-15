e208a84f17a0e9465d8e8188a46b31e821fb09d4
Samantha Brown <bro7445@gmail.com>
2021-06-15 10:19:53 -0400

    Establish game functions to be created

--- game.js
+++ game.js
@@ -2,25 +2,34 @@ class Game {
   constructor(player1, player2) {
     this.player1 = new Player('one');
     this.player2 = new Player('two');
-    this.whoseTurn = this.player; // needs to be player1/2??
+    this.whoseTurn = this.player1;
     this.winningBoards = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9],[3,5,7]]; // maybe object instead
   }
   changingPlayers() {
     if (this.whoseTurn === this.player1) {
-      this.activePlayer = this.player2;
+      this.whoseTurn = this.player2;
+    } else {
+      this.whoseTurn = this.player1;
     }
   }
 
   gameWinner() {
-    var winningPlayer =
+    if ()
   }
+  checkForDraw() {
 
+  }
+  checkForWin() {
+
+  }
+  resetBoard() {
+    if 
+  }
 }
 
 
 // will need methods for:
-//1. changing players,
+
 //2. logging tokins on board,
-// 3. determine winner of game, or if draw
+
 //4. displaying each players wins,
-//5. reseting board after game,
